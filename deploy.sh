83801914d87f127027b565e33c53a65bdd06dc0a
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-11-04 09:36:10 +0000

    changing branch

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ git init
 git remote add origin git@github.com:ahmednuaman/ahmednuaman.github.io.git
 git add .
 git commit -a -m 'deploy, yo'
-git branch gh-pages
-git checkout gh-pages
+git branch master
+git checkout master
 git branch -D master
-git push origin gh-pages -f
+git push origin master -f
 
 echo 'Deployed, yo'
