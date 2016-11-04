1b7d083277bd4d0067c1a464a48dd3c7c98e9780
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-11-04 09:39:49 +0000

    no need to delete

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,7 @@ git init
 git remote add origin git@github.com:ahmednuaman/ahmednuaman.github.io.git
 git add .
 git commit -a -m 'deploy, yo'
-git branch master
 git checkout master
-git branch -D master
 git push origin master -f
 
 echo 'Deployed, yo'
